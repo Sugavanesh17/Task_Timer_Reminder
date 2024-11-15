4a0e3aefecd3c57cb3794a9887b6eed1f5f909c7
SUGAVANESHWARAN S <154986511+Sugavanesh17@users.noreply.github.com>
2024-11-15 14:39:47 +0530

    Updated DatabaseConnection.java

--- TaskTimerReminderProject/src/database/DatabaseConnection.java
+++ TaskTimerReminderProject/src/database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/taskdb";
+    private static final String URL = "jdbc:mysql://localhost:3306/<Your_Database_name>";
     private static final String USER = "root";
-    private static final String PASSWORD = "MYSQLsugu";
+    private static final String PASSWORD = "<PASSWORD>";
 
     public static Connection getConnection() throws SQLException {
         try {
